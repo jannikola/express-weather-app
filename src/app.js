481f71fe54d2fbc3cbc1521848984c95c75f474d
jannikola <jannikola@gmail.com>
2020-01-26 22:15:49 +0100

    serving up HTML and JSON

--- src/app.js
+++ src/app.js
@@ -3,19 +3,27 @@ const express = require('express')
 const app = express()
 
 app.get('', (req, res) => {
-    res.send('Hello express!')
+    res.send('<h1>Weather</h1>')
 })
 
 app.get('/help', (req, res) => {
-    res.send('Help page!')
+    res.send([{
+        name: 'Nikola',
+        age: 27
+    },{
+        lastname: 'Jankovic',
+    }])
 })
 
 app.get('/about', (req, res) => {
-    res.send('About page!')
+    res.send('<h1>About page!</h1>')
 })
 
 app.get('/weather', (req, res) => {
-    res.send('Weather page!')
+    res.send({
+        forecast: 'It is raining',
+        location: 'Sarajevo'
+    })
 })
 
 app.listen(3000, () => {

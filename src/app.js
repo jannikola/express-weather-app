4ca61fb7c90996d5b92d22ced14000b687510e99
jannikola <jannikola@gmail.com>
2020-01-26 23:08:13 +0100

    dynamic pages with templating - handlebars

--- src/app.js
+++ src/app.js
@@ -4,7 +4,28 @@ const express = require('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Nikola Jankovic'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About page',
+        name: 'Nikola Jankovic'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'Help pls',
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({

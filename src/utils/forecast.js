0d1ec4ef2fb28dca49f01d65db2107d5dfc760db
jannikola <jannikola@gmail.com>
2020-10-18 20:20:50 +0200

    setup ssh test

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,6 +3,8 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/355a282dd033bb591ade9454ef9ca965/' + latitude + ',' + longitude
 
+    console.log('test ssh setup')
+
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
